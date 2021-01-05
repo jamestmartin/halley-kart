e938f6a5792570b80c922ee6e54a5ab6c8d209a2
James Martin <james@jtmar.me>
2021-01-05 13:50:16 -0800

    Initialize Vulkan, all the way up to making a logical device+queues.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+mod graphics;
+
 fn main() {
-    stderrlog::new().verbosity(2).init().unwrap();
-    
+    stderrlog::new().verbosity(4).init().unwrap();
+
     use winit::event_loop::EventLoop;
     let event_loop = EventLoop::new();
-    
+
     use winit::window::WindowBuilder;
     let window = WindowBuilder::new()
         // TODO: Make the window resizable
@@ -14,12 +16,17 @@ fn main() {
         // TODO: Support fullscreen.
         .build(&event_loop)
         .expect("Failed to create window.");
-        
+
+    let instance = graphics::vulkan::create_instance();
+    let surface = vulkano_win::create_vk_surface(window, instance.clone()).expect("Failed to create Vulkan surface for window.");
+    let queue_families = graphics::vulkan::select_physical_device(&instance, &surface);
+    let device = graphics::vulkan::create_device(queue_families);
+
     event_loop.run(|event, _, control_flow| {
         use winit::event::Event::*;
         use winit::event::WindowEvent::CloseRequested;
         use winit::event_loop::ControlFlow;
-        
+
         match event {
             WindowEvent { event: CloseRequested, .. } => {
                 *control_flow = ControlFlow::Exit;
